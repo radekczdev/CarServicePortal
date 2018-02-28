a1e0d047be380c2f1bfc84864ed950e02939074e
radekcz.dev <radekcz.dev@gmail.com>
2018-02-28 18:27:56 +0100

    Modified CascadeTypes for using only one DAO repository for saving RepairOrder

---
+++ src/main/java/com/czajor/carserviceportal/api/RepairOrderController.java
@@ -0,0 +1,10 @@
+package com.czajor.carserviceportal.crud;
+
+public class RepairOrderController {
+//    public List<RepairOrderDto> getRepairOrders();
+//    public RepairOrderDto getRepairOrder(int id);
+//    public void changeStatus(int id);
+//    public void createRepairOrder();
+//    public void createCar();
+//    public void createCustomer();
+}
