c8a8c5d4a800cf68c6a7b16c858a8e72f85e7a3a
radekcz.dev <radekcz.dev@gmail.com>
2018-03-06 21:37:18 +0100

    Added PostgreSQL data in application.properties

--- src/main/java/com/czajor/carserviceportal/SpringRunner.java
+++ src/main/java/com/czajor/carserviceportal/SpringRunner.java
@@ -2,10 +2,9 @@ package com.czajor.carserviceportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringRunner extends SpringBootServletInitializer {
+public class SpringRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringRunner.class, args);
     }
