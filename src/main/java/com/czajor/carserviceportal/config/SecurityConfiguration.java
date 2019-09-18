0252fb6b72bf3486600e8bf5af4d2a6bd1f8263a
radekczdev <radekcz.dev@gmail.com>
2019-09-18 08:07:51 +0200

    Added simple configuration for testing (basic auth, disabled csrf for now)

--- src/main/java/com/czajor/carserviceportal/config/SecurityConfiguration.java
+++ src/main/java/com/czajor/carserviceportal/config/SecurityConfiguration.java
@@ -1,10 +1,24 @@
 package com.czajor.carserviceportal.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER").and()
+				.withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+	}
+
+	// TODO implement csrf
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+	}
 }
