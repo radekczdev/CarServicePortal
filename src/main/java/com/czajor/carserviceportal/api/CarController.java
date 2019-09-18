936aa0f3cbf2f9d92b83d22bfb4e9986ea35a857
radekczdev <radekcz.dev@gmail.com>
2019-09-18 08:07:01 +0200

    Modified annotations for requests Modified method modifyCar to handle the exceptions caused by wrong id  - licensePlate

--- src/main/java/com/czajor/carserviceportal/api/CarController.java
+++ src/main/java/com/czajor/carserviceportal/api/CarController.java
@@ -1,11 +1,16 @@
 package com.czajor.carserviceportal.api;
 
 import com.czajor.carserviceportal.domain.CarDto;
+import com.czajor.carserviceportal.exception.CarNotFoundException;
 import com.czajor.carserviceportal.mapper.CarMapper;
+import com.czajor.carserviceportal.model.Car;
 import com.czajor.carserviceportal.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
@@ -21,22 +26,33 @@ public class CarController {
     @Autowired
     private CarMapper carMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{carId}")
+    @GetMapping(value = "/{carId}")
     public CarDto getCar(@PathVariable("carId") final String carId) {
         return carMapper.mapToCarDto(carService.getCar(carId));
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<CarDto> getCars() {
         return carMapper.mapToCarDtoList(carService.getCars());
     }
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = APPLICATION_JSON_VALUE)
-    public void modifyCar(@RequestBody final CarDto carDto) {
-        carService.modifyCar(carDto);
+    @PutMapping(consumes = APPLICATION_JSON_VALUE)
+    public CarDto modifyCar(@RequestBody final CarDto carDto, HttpServletResponse response) {
+        try {
+            Car car = carService.modifyCar(carDto);
+            return carMapper.mapToCarDto(car);
+        } catch (IllegalAccessException e1) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Wrong DTO object provided!"
+            );
+        } catch (CarNotFoundException e2) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Car not found."
+            );
+        }
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{licensePlate}")
+    @DeleteMapping(value = "/{licensePlate}")
     public void deleteCar(@PathVariable("licensePlate") final String licensePlate) {
         carService.deleteCar(licensePlate);
     }
